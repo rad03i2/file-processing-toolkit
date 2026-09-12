281120a77144b7e7dca9868c9cc658f7aa0cdde2
Radwan Abdulhadi Ahmed <127021904+rad03i2@users.noreply.github.com>
2026-09-12 23:51:03 +0300

    Add C# file hasher

---
+++ csharp/FileHasher.cs
@@ -0,0 +1,13 @@
+using System.Security.Cryptography;
+
+namespace FileProcessingToolkit;
+
+public static class FileHasher
+{
+    public static async Task<string> Sha256Async(string path)
+    {
+        await using var stream = File.OpenRead(path);
+        var hash = await SHA256.HashDataAsync(stream);
+        return Convert.ToHexString(hash).ToLowerInvariant();
+    }
+}
